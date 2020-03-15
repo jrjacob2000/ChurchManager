39f5da13c84f0e7ea02ba5eb3dae9f03b4c4a2a3
Jonathan Jacob <jonathanj@gmail.com>
2020-03-15 10:40:41 +0800

    add GetLineChart endpoit

--- ChurchManager/Controllers/HomeController.cs
+++ ChurchManager/Controllers/HomeController.cs
@@ -6,13 +6,13 @@ using System.Web.Mvc;
 
 namespace ChurchManager.Controllers
 {
-    
+
     public class HomeController : Controller
     {
-  
+
         public ActionResult Index()
         {
-            
+
             return View();
         }
 
@@ -28,6 +28,23 @@ namespace ChurchManager.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public JsonResult GetLineChart()
+        {
+            var result = new Entry[] {
+                new Entry() { value = 200, month = "2019-01" },
+                new Entry() { value = 200, month = "2019-01" },
+                new Entry() { value = 200, month = "2019-01" },
+            };
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        private class Entry
+        {
+            public int value { get; set; }
+            public string month { get; set; }
         }
     }
 }
